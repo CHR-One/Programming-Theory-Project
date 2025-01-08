a8214974d8c1196f314353ebdf3f7cc81acf042d
Cristian Palmas <cristian.palmas@gmail.com>
2025-01-08 13:52:49 +0100

    Reversed to original code in InitialSquare
    - Original cycle saved again to change implementation

--- Assets/Scripts/InitialSquare.cs
+++ Assets/Scripts/InitialSquare.cs
@@ -25,31 +25,30 @@ public class InitialSquare : Square
     public override void FillBoxes()
     {
         remainingNumbers.AddRange(Numbers);
-
+        
         for (int k = 0; k < 3; k++)
         {
             for (int j = 0; j < 3; j++)
-            {
+            {                
                 Debug.Log($"STARTING cycle [{k}, {j}]");
                 Debug.Log("remainingNumbers: { " + string.Join(", ", remainingNumbers) + " }");
-                if (k == 2 && j == 1 && sudoku.columns[2].Contains(remainingNumbers[0]))
-                {
-                    possibleNumbers = remainingNumbers;
-                }                
-                
-                possibleNumbers = remainingNumbers.Except(sudoku.columns[j]).ToList();
+                possibleNumbers = remainingNumbers.Except(sudoku.columns[j]).ToList();                
+
                 Debug.Log("possibleNumbers: { " + string.Join(", ", possibleNumbers) + " }");
-                ShuffleList(possibleNumbers);                
+                if (possibleNumbers.Count != 1)
+                    ShuffleList(possibleNumbers);
+
+                Debug.Log("possibleNumbers shuffled: { " + string.Join(", ", possibleNumbers) + " }");
                 square.grid[k, j] = possibleNumbers.First();
                 Debug.Log($"grid[{k}, {j}]: {grid[k, j]}");
                 sudoku.columns[j].Add(square.grid[k, j]);
                 Debug.Log($"columns[{j}]: {{ {string.Join(", ", sudoku.columns[j])} }}");
-                remainingNumbers.Remove(square.grid[k, j]);                
+                remainingNumbers.Remove(square.grid[k, j]);
                 boxes[j + 3 * k].GetComponentInChildren<TextMeshProUGUI>().text = square.grid[k, j].ToString();
                 Debug.Log("remainingNumbers: { " + string.Join(", ", remainingNumbers) + " }");
                 Debug.Log($"End of cycle [{k}, {j}]");
                 Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             }
-        }        
+        }
     }
 }
